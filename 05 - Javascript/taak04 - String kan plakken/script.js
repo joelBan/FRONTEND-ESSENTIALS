4cc623aee3c04ad859fc8eaed17f816ae13eac78
Joël Banshi <Joel.banshi@student.rocva.nl>
2021-04-08 13:26:04 +0200

    snel

---
+++ 05 - Javascript/taak04 - String kan plakken/script.js
@@ -0,0 +1,6 @@
+let Mijnvoornaam= "Joel"
+let Mijnachternaam= "BAnshi"
+let Mijnleeftijd= 16;
+
+alert(Mijnvoornaam + Mijnachternaam);
+    console.log("Mijn naam is `<voornaam>` `<achternaam>` en mijn leeftijd is `<leeftijd>` jaar");
