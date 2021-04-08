8f78d442def5fdd230e02ca0c9f4d5169cefc938
Joël Banshi <Joel.banshi@student.rocva.nl>
2021-04-08 12:36:27 +0200

    taak 3

---
+++ 05 - Javascript/taak03 - Datatype Object/script.js
@@ -0,0 +1,14 @@
+
+
+let student = {
+    voornaam: "Joel",
+    achternaam: "Banshi",
+    leeftijd: 16,
+    ikBenStemGerechtig: false,
+    ikMagLegaalAlcoholKopen: true,
+    ikMagBrommerRijden: true,
+}
+ 
+console.log(student);
+
+console.log(student.leeftijd);
