73f5fa43e35bb61315e3b780ff0c8108fb661304
Joël Banshi <Joel.banshi@student.rocva.nl>
2021-04-08 12:21:56 +0200

    taak 2 gedaan

---
+++ 05 - Javascript/taak02 - Op welke type let jij/script.js
@@ -0,0 +1,15 @@
+
+
+let voornaam="Joël";
+let achternaam="Banshi";
+let leeftijd= 17;
+let ikBenStemGerechtig= false;
+let ikMagLegaalAlcoholKopen= false;
+let ikMagBrommerRijden= true;
+let SoftwareOntwikkeling= null;
+
+console.log(SoftwareOntwikkeling);
+console.log(voornaam);
+
+leeftijd= leeftijd-13;
+console.log(leeftijd)
