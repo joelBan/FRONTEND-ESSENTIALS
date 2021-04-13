ecb07a9488e31e01d3101bc036d53a7e62dc9791
Joël Banshi <Joel.banshi@student.rocva.nl>
2021-04-13 12:03:00 +0200

    commit

---
+++ 05 - Javascript/taak05 - As if/script.js
@@ -0,0 +1,7 @@
+let mijnLeeftijd = 18;
+let stemGerechtigeLeeftijd= 18;
+
+if(mijnLeeftijd==stemGerechtigeLeeftijd){
+    console.log("Jij bent mag stemmen met de verkiezingen");
+    alert("Jij bent mag stemmen met de verkiezingen");
+}
